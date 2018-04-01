9ddade04917fb7fd0df44eb362f38f7299c9593a
Brandon David <brandonhudavid@berkeley.edu>
2018-03-31 19:14:11 -0700

    added files

---
+++ gcp/speech-client-library.py
@@ -0,0 +1,39 @@
+import io
+import os
+
+# Imports the Google Cloud client library
+from google.cloud import speech
+from google.cloud.speech import enums
+from google.cloud.speech import types
+
+# Instantiates a client
+client = speech.SpeechClient()
+print("instantiated")
+
+# The name of the audio file to transcribe
+file_name = os.path.join(
+    os.path.dirname(__file__),
+    'resources',
+    'test.wav')
+print(file_name)
+
+# Loads the audio into memory
+with io.open(file_name, 'rb') as audio_file:
+    content = audio_file.read()
+    audio = types.RecognitionAudio(content=content)
+
+
+config = types.RecognitionConfig(
+    encoding=enums.RecognitionConfig.AudioEncoding.LINEAR16,
+    sample_rate_hertz=16000,
+    language_code='en-US')
+
+print("before response")
+# Detects speech in the audio file
+response = client.recognize(config, audio)
+print("after response")
+print(response)
+
+for result in response.results:
+    print('Transcript: {}'.format(result.alternatives[0].transcript))
+print("finished")
